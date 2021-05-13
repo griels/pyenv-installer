7d97e1d602d3cefc16dbb536de6f482a3eabdf0b
Ellis Breen <ebreen@pushtechnology.com>
2021-05-13 11:35:19 +0100

    Add pyenv.run

---
+++ pyenv.run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Usage: curl https://pyenv.run | bash
+#
+# For more info, visit: https://raw.githubusercontent.com/pyenv/pyenv-installer
+#
+
+PYENV_REPO="pyenv"
+
+index_main() {
+    set -e
+    curl -s -S -L https://raw.githubusercontent.com/${PYENV_REPO}/master/bin/pyenv-installer | bash
+}
+
+if [ -z "$PYENV_INSTALLING" ]; then
+  index_main
+fi
